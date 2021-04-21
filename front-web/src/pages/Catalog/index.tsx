267eff8a418889a0975200930c0b75f2727bb16d
Marcio Gama <marciogama2000@gmail.com>
2021-04-21 13:00:05 -0300

    Adding loader in product details

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './Components/ProductCard';
+import ProductCardLoader from './Components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
     // quando a lista de produtos estiver disponível,
     // popular um estado no componente, e listar os produtos dinâmicamente
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
 
     console.log(productsResponse);
 
@@ -17,22 +19,29 @@ const Catalog = () => {
             page: 0,
             linesPerPage: 12
         }
-
+        // iniciar o loader
+        setIsLoading(true);
         makeRequest({ url: '/products', params })
-            .then(response => setProductsResponse(response.data));
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                // finalizar o loader
+                setIsLoading(false);
+            });
     }, [])
 
-    return(
+    return (
         <div className="catalog-container">
             <h1 className="catalog-title">
                 Catalogo de produtos
             </h1>
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
-                    <Link to={`/products/${product.id}`} key={product.id}>
-                        <ProductCard product={product} />
-                    </Link>
-                ))}
+                {isLoading ? <ProductCardLoader /> :
+                    (productsResponse?.content.map(product => (
+                        <Link to={`/products/${product.id}`} key={product.id}>
+                            <ProductCard product={product} />
+                        </Link>
+                    ))
+                )}
             </div>
         </div>
     );
