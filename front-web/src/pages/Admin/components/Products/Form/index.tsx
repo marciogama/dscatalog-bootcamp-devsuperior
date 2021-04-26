f465a0b4ac62194a9bb917395546e86c0db3da4d
Marcio Gama <marciogama2000@gmail.com>
2021-04-26 12:01:33 -0300

    Created generic structure to deal with serverals form datas

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+type FormState = {
+    name: string;
+    price: string;
+    category: string;
+}
+
+
 const Form = () => {
+    const [formData, setFormData] = useState<FormState>({
+        name: 'Computador',
+        price: '',
+        category: 'eletronicos'
+    });
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        
+        setFormData(data => ({...data, [name]: value}));
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log(formData)
+    }
+
     return (
-       <BaseForm title="CADASTAR UM PRODUTO">
-           <div className="row">
-               <div className="col-6">
-                   <input type="text" className="form-control"/>
-               </div>
-           </div>
-       </BaseForm>
-     
+        <form onSubmit={handleSubmit}>
+            <BaseForm title="cadastrar um produto">
+                <div className="row">
+                    <div className="col-6">
+                        <input
+                            value={formData.name}
+                            name="name"
+                            type="text"
+                            className="form-control mb-5"
+                            onChange={handleOnChange}
+                            placeholder="Nome do produto"
+                        />
+                        <select
+                            value={formData.category}
+                            className="form-control mb-5"
+                            onChange={handleOnChange}
+                            name="category"
+                        >
+                            <option value="livros">Livros</option>
+                            <option value="computadores">Computadores</option>
+                            <option value="eletronicos">Eletrônicos</option>
+                        </select>
+                        <input
+                            value={formData.price}
+                            name="price"
+                            type="text"
+                            className="form-control"
+                            onChange={handleOnChange}
+                            placeholder="Preço"
+                        />
+                    </div>
+                </div>
+            </BaseForm>
+        </form>
+
     )
 }
 
