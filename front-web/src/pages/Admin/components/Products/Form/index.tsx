1fc26e3f94ade7a76d43bb9b71596010647eb6a7
Marcio Gama <marciogama2000@gmail.com>
2021-05-10 11:28:32 -0300

    insert product in backend

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,3 +1,4 @@
+import { makeRequest } from 'core/utils/request';
 import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
@@ -6,17 +7,20 @@ type FormState = {
     name: string;
     price: string;
     category: string;
+    description: string;
 }
 
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
 
 const Form = () => {
     const [formData, setFormData] = useState<FormState>({
         name: 'Computador',
         price: '',
-        category: 'eletronicos'
+        category: 'eletronicos',
+        description: ''
     });
 
-    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const handleOnChange = (event: FormEvent) => {
         const name = event.target.name;
         const value = event.target.value;
         
@@ -25,8 +29,15 @@ const Form = () => {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
-        console.log(formData)
+        const payload = {
+            ...formData,
+            imgUrl: 'https://static.netshoes.com.br/produtos/console-playstation-5-ps5-sony/14/D32-2445-014/D32-2445-014_zoom1.jpg?ts=1602777382',
+            categories: [{ id: formData.category }]
+        };
+        makeRequest({ url: '/products', method: 'POST', data: payload })
+            .then(() => {
+                setFormData({ name: '', category: '', price: '', description: ''});
+            });
     }
 
     return (
@@ -48,9 +59,9 @@ const Form = () => {
                             onChange={handleOnChange}
                             name="category"
                         >
-                            <option value="livros">Livros</option>
-                            <option value="computadores">Computadores</option>
-                            <option value="eletronicos">Eletrônicos</option>
+                            <option value="1">Livros</option>
+                            <option value="3">Computadores</option>
+                            <option value="2">Eletrônicos</option>
                         </select>
                         <input
                             value={formData.price}
@@ -59,6 +70,16 @@ const Form = () => {
                             className="form-control"
                             onChange={handleOnChange}
                             placeholder="Preço"
+                        />
+                    </div>
+                    <div className="col-6">
+                        <textarea 
+                            name="description" 
+                            value={formData.description}
+                            onChange={handleOnChange}
+                            className="form-control"
+                            cols={30} 
+                            rows={10}
                         />
                     </div>
                 </div>
